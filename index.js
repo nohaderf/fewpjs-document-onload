9ac300994b35c5257348e3b8fd8736a5d9430aad
nohaderf <freda.hon@gmail.com>
2020-11-17 16:30:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+function updateDOM(){
+    const text = document.querySelector("#text")
+    text.textContent = "This is really cool!"
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    updateDOM()
+    console.log("DOM LOADED")
+})
+
